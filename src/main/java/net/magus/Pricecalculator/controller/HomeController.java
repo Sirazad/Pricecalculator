bef6d30a38bd091e54103a5a538557acd5399666
Imola Saraine Lajos <Imola_Saraine_Lajos@epam.com>
2022-04-18 13:51:27 +0200

    add home and index page

--- src/main/java/net/magus/Pricecalculator/controller/HomeController.java
+++ src/main/java/net/magus/Pricecalculator/controller/HomeController.java
@@ -2,20 +2,24 @@ package net.magus.Pricecalculator.controller;
 
 import net.magus.Pricecalculator.service.ProductTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @Autowired
     ProductTypeService service;
 
     @GetMapping("/")
     public String homepage() {
-        String text = "<html><body>"+
-                "<h1>This is the price calculator base page </h1></b> "+
-                "the products are: <p/>"+
-                service.getAllProductTypes().toString();
-        return text;
+        return "home";
+    }
+
+    @GetMapping("/index")
+    public String showProductTypeList(Model model) {
+        model.addAttribute("categories", service.getAllProductTypes());
+        return "index";
     }
 }
